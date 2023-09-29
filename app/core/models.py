07eeefd8e84a7481c0fa23d2e58b8d82389840ff
Diego Alencar Jacober <57761933898>
2023-09-29 16:12:22 -0300

    Require email input

--- app/core/models.py
+++ app/core/models.py
@@ -13,6 +13,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must be an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
