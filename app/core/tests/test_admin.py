42a0db6cfc743626abadbcc68aa1a625c73cef0e
Diego Alencar Jacober <57761933898>
2023-09-29 16:44:18 -0300

    Test for listing users

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,35 @@
+"""
+Tests for django admin.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """Tests for django admin."""
+
+    def setUp(self):
+        """Create user and client."""
+        # https://docs.djangoproject.com/en/3.2/topics/testing/tools/#overview-and-a-quick-example
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="testpass123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
+
+    def test_users_list(self):
+        """Test that users are listed on page."""
+        # https://docs.djangoproject.com/en/3.1/ref/contrib/admin/#reversing-admin-urls
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
