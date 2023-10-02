8eab147d9f5983060766a263ecad29479193b3b8
Diego Alencar Jacober <57761933898>
2023-10-02 15:46:59 -0300

    Implement manage user API

--- app/user/urls.py
+++ app/user/urls.py
@@ -9,5 +9,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('create', views.CreateUserView.as_view(), name='create'),
-    path('token/', views.CreateTokenView.as_view(), name='token')
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManagerUserView.as_view(), name='me'),
 ]
