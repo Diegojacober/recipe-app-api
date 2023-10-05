486de48b6faed8003626855615e4ae9c8d56c5ac
Diegojacober <diegoalencar.jacober@gmail.com>
2023-10-05 15:58:32 -0300

    Implementing ingredient model

--- app/core/models.py
+++ app/core/models.py
@@ -55,6 +55,18 @@ class Tag(models.Model):
         return self.name
 
 
+class Ingredient(models.Model):
+    """Ingredient for recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
     """Recipe object."""
     user = models.ForeignKey(
@@ -67,6 +79,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField(Tag, blank=True, default='')
+    ingredients = models.ManyToManyField(Ingredient)
 
     def __str__(self) -> str:
         return self.title
