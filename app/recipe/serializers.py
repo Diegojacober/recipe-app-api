a0e4a2b1a56e77ac1f35ae61430e3e34dc382869
Diegojacober <diegoalencar.jacober@gmail.com>
2023-10-05 16:19:30 -0300

    Fixing bugs in route

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -51,7 +51,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes',
-                  'price', 'link', 'tags']
+                  'price', 'link', 'tags', 'ingredients']
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -13,6 +13,7 @@ from recipe import views
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
 router.register('tags', views.TagViewset)
+router.register('ingredients', views.IngredientViewset)
 
 app_name = 'recipe'
 
