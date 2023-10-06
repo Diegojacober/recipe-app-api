84ad945573f37bd8f6409de2d0b242d511689feb
Diego <diegoalencar.jacober@gmail.ocm>
2023-10-06 19:35:51 -0300

    Fixing filter bug

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -52,23 +52,23 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return [int(str_id) for str_id in qs.split(',')]
 
     def get_queryset(self):
-        """Retrieve recipes for authenticated user"""
-
+        """Retrieve recipes for authenticated user."""
         tags = self.request.query_params.get('tags')
         ingredients = self.request.query_params.get('ingredients')
         queryset = self.queryset
         if tags:
-            tags_ids = self._params_to_ints(tags)
-            queryset = queryset.filter(tags__id__in=tags_ids)
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
         if ingredients:
-            ingredients_ids = self._params_to_ints(ingredients)
-            queryset = queryset.filter(ingredients__id__in=ingredients_ids)
-        return self.queryset.filter(
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(
             user=self.request.user
         ).order_by('-id').distinct()
 
     def get_serializer_class(self):
-        """Return the serializer class for the request"""
+        """Return the serializer class for request."""
         if self.action == 'list':
             return serializers.RecipeSerializer
         elif self.action == 'upload_image':
