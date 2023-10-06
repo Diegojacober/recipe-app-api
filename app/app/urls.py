04344f877d1881a115f2a6df0eebfd650a178752
Diegojacober <diegoalencar.jacober@gmail.com>
2023-10-06 14:04:25 -0300

    Configure project for static files

--- app/app/urls.py
+++ app/app/urls.py
@@ -8,6 +8,8 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,10 @@ urlpatterns = [
          name='api-docs'
          ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
+    
